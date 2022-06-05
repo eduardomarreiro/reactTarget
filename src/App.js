63fee404191f8d1b2abfb46e99257c57a3267a2f
eduardomarreiro <eduardo4pc@outlook.com>
2022-06-05 11:27:30 -0300

    front-end with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useEffect, useEffectFrom, useState } from 'react';
 import './App.css';
+import'bootstrap/dist/css/bootstrap.min.css';
+import axios from'axios';
+import{Modal,ModalBody,ModalFooter,ModalHeader}from'reactstrap';
+
+
+
 
 function App() {
+
+  const baseUrl = "https://localhost:44316/api/person";
+
+  const [data, setData] = useState([]);
+
+  const askGet = async() => {
+    await axios.get(baseUrl)
+    .then(response => {
+      setData(response.data);
+    }).catch(error =>{
+      console.log(error);
+    })
+  }
+
+  useEffect(() =>{
+    askGet();
+  })
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <br/>
+      <h3>Person registration</h3>
+      <header>
+        
+        <button className="btn btn-success">Add person</button> 
       </header>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Username</th>
+            <th>FullName</th>
+            <th>Born</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Options</th>
+          </tr>
+        </thead>
+        <tbody>
+        {data.map(person=>(
+        <tr key={person.Id}>
+          <td>{person.Id}</td>
+          <td>{person.Username}</td>
+          <td>{person.FullName}</td>
+          <td>{person.Born}</td>
+          <td>{person.Email}</td>
+          <td>{person.Phone}</td>
+          <td>
+            <button className="btn btn-primary">Edit</button>{""}
+            <button className="btn btn-danger">Delete</button>
+          </td>
+        </tr>
+        ))}
+        </tbody>
+      </table>
     </div>
   );
 }
